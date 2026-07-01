5652ecb86bf20300aaffe09d9d99b3c3c8bbb850
wm87 <mwf102.mw@gmail.com>
2026-07-01 19:04:01 +0200

    Upgrade: Add Pomelo MySQL provider, fix App.xaml.cs using; add unit test project

---
+++ WpfApp.Tests/ExampleTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace WpfApp.Tests
+{
+    public class ExampleTests
+    {
+        [Fact]
+        public void SmokeTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
--- WpfApp/App.xaml.cs
+++ WpfApp/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
